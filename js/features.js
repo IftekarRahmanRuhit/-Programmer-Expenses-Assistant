558da7f88d1f166c11e3c7d49a75072288f518f5
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-22 00:55:03 +0600

    updated

--- js/features.js
+++ js/features.js
@@ -1,10 +1,6 @@
+
 document.getElementById('calculate').addEventListener('click',function(){
     document.getElementById('results').classList.remove('hidden');
-
-
-
-
-
 })
 
 document.getElementById('history-tab').addEventListener('click',function(){
@@ -19,6 +15,18 @@ document.getElementById('history-tab').addEventListener('click',function(){
         document.getElementById('history-section').classList.remove('hidden');
         document.getElementById('history-list').classList.remove('hidden');
 
+})
 
+document.getElementById('assistant-tab').addEventListener('click',function(){
+    document.getElementById('assistant-tab').classList.add(
+        'text-white','bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
 
+        document.getElementById('history-tab').classList.remove( 
+            'text-white','bg-gradient-to-r', 'from-blue-500', 'to-purple-600'
+        );
+        
+        document.getElementById('expense-form').classList.remove('hidden');
+        document.getElementById('results').classList.add('hidden');
+        document.getElementById('history-section').classList.add('hidden');
+        document.getElementById('history-list').classList.add('hidden');
 })
